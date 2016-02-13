a75b3f0f5882ed293d0c8e1c829320c3f828b3bd
Kyle Conlon <conlonkyle@gmail.com>
2016-02-12 21:32:27 -0500

    Setting the app up against a CSRF attack

--- brew_journal/static/js/brew_journal.js
+++ brew_journal/static/js/brew_journal.js
@@ -1,1 +1,27 @@
-var brewJournal = angular.module("brewJournal", ["ui.router"]);
+(function () {
+	'use strict';
+
+	angular
+		.module('brewJournal',[
+			// Future modules go here
+		]);
+
+	angular
+		.module('brewJournal.routes', ['ngRoute']);
+
+	// Protect against CSFR attacks
+	angular
+		.module('brewJournal')
+		.run(run);
+
+	run.$inject = ['$http'];
+
+	/**
+	* @name run
+	* @desc Update xsr $http headers to align with Django's defaults
+	*/
+	function run($http) {
+		$http.defaults.xsrfHeaderName = 'X-CSRFToken';
+		$http.defaults.xsrfCookieName = 'csrftoken';
+	}
+})();
