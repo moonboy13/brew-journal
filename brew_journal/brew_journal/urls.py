28604717d1cc18f29538f26a3557a3ed6ca33b05
Kyle Conlon <conlonkyle@gmail.com>
2016-02-13 13:51:18 -0500

    Setup URL routing for the Django side of the house

--- brew_journal/brew_journal/urls.py
+++ brew_journal/brew_journal/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+
+from brew_journal.views import IndexView
+
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet, LoginView
+
+router = routers.SimpleRouter()
+router.register(r'register', AccountViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'brew_journal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+
+    # Default index view. Must be last to avoid accidentially catching other URLs
+    url('^.*$', IndexView.as_view(), name='index'),
 )
