b31ae4dd37b41cbedbf624a8dfe62c5bfd3cb5b7
Kyle Conlon <conlonkyle@gmail.com>
2016-02-23 21:13:14 -0500

    Fleshed out the recipe table. Remove the gravity checks table as that will be part of the batch set of tables

--- brew_journal/recipies/models.py
+++ brew_journal/recipies/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Recipe model to gather all of the ingredients details together
 class Recipe(model.Models):
   """Contain all of a user's recipies"""
-  recipe_name = models.CharField(max_length=60)
+  recipe_name  = models.CharField(max_length=60)
+  recipe_style = models.CharField(max_length=140)
+  recipe_notes = models.TextField()
 
   date_created   = models.DateTimeField(auto_now_add=True)
   date_updated   = models.DateTimeField(auto_now=True)
@@ -17,7 +19,4 @@ class RecipeMalts(model.Models):
 
 class RecipeHops(model.Models):
   """Table for each of the hops in a recipe"""
-
-class RecipeGravityChecks(model.Models):
-  """Holder for each gravity check and the percent alcohol at that date"""
 
