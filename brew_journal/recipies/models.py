d7c012f922605f365a0b331ace0e521d2222c641
Kyle Conlon <conlonkyle@gmail.com>
2016-02-23 21:05:36 -0500

    Added the base classes for all the models. Only added a few of the fields

--- brew_journal/recipies/models.py
+++ brew_journal/recipies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Recipe model to gather all of the ingredients details together
+class Recipe(model.Models):
+  """Contain all of a user's recipies"""
+  recipe_name = models.CharField(max_length=60)
+
+  date_created   = models.DateTimeField(auto_now_add=True)
+  date_updated   = models.DateTimeField(auto_now=True)
+  last_brew_date = models.DateTimeField(blank=True)
+
+class RecipeMalts(model.Models):
+  """Hold descriptors for all the malts used in a recipe"""
+  malt_name = models.CharField(max_length=120)
+
+  amount_by_weight = models.DecimalField(max_digits=10, decimal_places=3)
+
+class RecipeHops(model.Models):
+  """Table for each of the hops in a recipe"""
+
+class RecipeGravityChecks(model.Models):
+  """Holder for each gravity check and the percent alcohol at that date"""
+
