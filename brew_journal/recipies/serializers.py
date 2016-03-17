9315358bbd72d7f95ffda92eb3a70499d42d08bc
Kyle Conlon <conlonkyle@gmail.com>
2016-03-16 20:27:57 -0400

    Incremental commit in setting up serializers for the recipe classes

--- brew_journal/recipies/serializers.py
+++ brew_journal/recipies/serializers.py
@@ -5,8 +5,23 @@ from recipies.models import Recipe, RecipeMalts, RecipeHops
 class RecipeSerializer(serializers.ModelSerializer):
   """Serialization class for all your yummy recipes."""
 
+  class Meta:
+    model = Recipe
+    fields = '__all__'
+
+    def create(self, validated_data):
+      """Create the recipe and all related data"""
+      hops  = validated_data.get('recipe_hops')
+      malts = validated_data.get('recipe_malts')
+
 class RecipeMaltsSerializer(serializers.ModelSerializer):
   """Subserializer for Recipes. Handles the malts."""
+  class Meta:
+    model = RecipeMalts
+    exclude = ('id',)
 
 class RecipeHopsSerializers(serializers.ModelSerializer):
   """Subserializer for Recipes. Handles the hops."""
+  class Meta:
+    model  = RecipeHops
+    exclude = ('id',)
