5439edd6f9c6656fabda70cc31f936080f3cd49b
Kyle Conlon <conlonkyle@gmail.com>
2016-03-16 20:49:14 -0400

    Completed setting up the serializer

--- brew_journal/recipies/serializers.py
+++ brew_journal/recipies/serializers.py
@@ -9,10 +9,16 @@ class RecipeSerializer(serializers.ModelSerializer):
     model = Recipe
     fields = '__all__'
 
-    def create(self, validated_data):
+    def create(self, validated_data, **kwargs):
       """Create the recipe and all related data"""
+      # As hops and malt will be handled separately, remove them from the current data.
       hops  = validated_data.get('recipe_hops')
+      del validated_data['recipe_hops']
       malts = validated_data.get('recipe_malts')
+      del validated_data['malts']
+      user = kwargs.get('user')
+
+      return Account.object.create_recipe(user, validated_data, malts, hops)
 
 class RecipeMaltsSerializer(serializers.ModelSerializer):
   """Subserializer for Recipes. Handles the malts."""
