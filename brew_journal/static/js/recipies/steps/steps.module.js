ec8a3feb08027c23ecfc7a014317d95365dd44af
Kyle Conlon <conlonkyle@gmail.com>
2016-07-29 20:23:47 -0400

    Beginnings of the front-end work to include the steps

---
+++ brew_journal/static/js/recipies/steps/steps.module.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('brew_journal.recipies.steps', [
+            'brew_journal.recipies.steps.controllers',
+            'brew_journal.recipies.steps.services'
+        ]);
+
+    angular
+        .module('brew_journal.recipies.steps.controllers' , []);
+    
+    angular
+        .module('brew_journal.recipies.steps.services' , []);
+})();
