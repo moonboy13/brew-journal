1c35ca45c1c627ecb53938042ec8bd4e3fb9441d
Kyle Conlon <conlonkyle@gmail.com>
2016-09-28 20:28:11 -0400

    More minor additions. Completely untested at this point

--- brew_journal/static/js/brew_journal.routes.js
+++ brew_journal/static/js/brew_journal.routes.js
@@ -1,29 +1,33 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('brew_journal.routes')
-    .config(config);
+    angular
+        .module('brew_journal.routes')
+        .config(config);
 
-  config.$inject = ['$routeProvider'];
+    config.$inject = ['$routeProvider'];
 
-  /**
-  * @name config
-  * @desc Define valid application routes
-  */
-  function config($routeProvider) {
-    $routeProvider.when('/view/register', {
-      controller:   'RegisterController',
-      controllerAs: 'ctrl',
-      templateUrl:  '/static/templates/authentication/register.html'
-    }).when('/view/login', {
-      controller:   'LoginController',
-      controllerAs: 'ctrl',
-      templateUrl:  '/static/templates/authentication/login.html'
-    }).when('/view/recipe', {
-      controller:   'RecipeController',
-      controllerAs: 'ctrl',
-      templateUrl:  '/static/templates/recipies/recipe.html'
-    }).otherwise('/view/login');
-  }
+    /**
+     * @name config
+     * @desc Define valid application routes
+     */
+    function config($routeProvider) {
+        $routeProvider.when('/view/register', {
+            controller:   'RegisterController',
+            controllerAs: 'ctrl',
+            templateUrl:  '/static/templates/authentication/register.html'
+        }).when('/view/login', {
+            controller:   'LoginController',
+            controllerAs: 'ctrl',
+            templateUrl:  '/static/templates/authentication/login.html'
+        }).when('/view/recipe', {
+            controller:   'RecipeController',
+            controllerAs: 'ctrl',
+            templateUrl:  '/static/templates/recipies/recipe.html'
+        }).when('/view/steps', {
+            controller:   'StepsController',
+            controllerAs: 'ctrl',
+            templateUrl:  '/static/templates/recipies/steps/steps.html'
+        }).otherwise('/view/login');
+    }
 })();
