758cc765e57b74e5f39f4eaad64228a4bec30d31
Kyle <conlonkyle@gmail.com>
2017-04-16 14:52:43 -0400

    Updated the database setup SQL to be a bit friendlier to find and replace

--- sql/data_base_setup.sql
+++ sql/data_base_setup.sql
@@ -14,7 +14,7 @@ ALTER ROLE <insert_roll> SET timezone TO 'UTC';
 
 --Grant you users some permissions
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO <insert_roll>;
-GRANT <insert_roll> TO <insert_other_role>;
+GRANT <insert_roll> TO <insert_user>;
 
 --For Django testing purposes, the created user you're connecing with needs the
 --permission to create a DB so Django can run tests on an isolated DB.
