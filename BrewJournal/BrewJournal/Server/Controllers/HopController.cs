fd69fd1463591f6ad5efa20bc3988c63a1b15d5d
Kyle Conlon <conlonkyle@gmail.com>
2020-12-12 16:53:56 -0500

    Adding some logging to the HopController

--- BrewJournal/BrewJournal/Server/Controllers/HopController.cs
+++ BrewJournal/BrewJournal/Server/Controllers/HopController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Models;
 using DatabaseConnector.Sqlite;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Text.Json;
 
 namespace BrewJournal.Server.Controllers
 {
@@ -26,6 +28,8 @@ namespace BrewJournal.Server.Controllers
 		[HttpGet]
 		public async Task<IEnumerable<Hop>> Index()
 		{
+			Stopwatch timer = GetAndStartTimer();
+			
 			await Task.CompletedTask;
 
 			var query = 
@@ -33,13 +37,20 @@ namespace BrewJournal.Server.Controllers
 				orderby hop.Name
 				select hop;
 
-			return query.ToList();
+			var hopList = query.ToList();
+
+			_logger.LogInformation("Retrieved all hops");
+			_logger.LogTrace($"Elapsed time to retrieve all hops {timer?.Elapsed.Seconds ?? 0}");
+			return hopList;
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create([Bind("Name,AlphaAcidContent,BetaAcidContent")] Hop hop)
 		{
+			//-- Only allocate space for and start a timer if we are running a trace log level
+			Stopwatch timer = GetAndStartTimer();
+
 			//-- Save to the database if the model is valid, otherwise return the validation errors to the UI
 			if(ModelState.IsValid)
 			{
@@ -47,17 +58,35 @@ namespace BrewJournal.Server.Controllers
 				var written = await _context.SaveAsync();
 				if (written > 0)
 				{
+					_logger.LogInformation("Hop Created", JsonSerializer.Serialize(hop));
+					_logger.LogTrace($"Time to create hop {timer?.Elapsed.Seconds ?? 0}");
 					return Ok(hop);
 				}
 				else
 				{
+					_logger.LogError("Database write failed when creating hop.", JsonSerializer.Serialize(hop));
+					_logger.LogTrace($"Time for create hop failure {timer?.Elapsed.Seconds ?? 0}");
 					return Problem("Unknown error occured when saving data");
 				}
 			}
 			else
 			{
+				_logger.LogInformation("Hop failed to validate.", JsonSerializer.Serialize(hop));
+				_logger.LogTrace($"Time for validation failure {timer?.Elapsed.Seconds ?? 0}");
 				return ValidationProblem(ModelState);
 			}
+		}
+
+		private Stopwatch GetAndStartTimer()
+		{
+			Stopwatch timer = null;
+			if(_logger.IsEnabled(LogLevel.Trace))
+			{
+				timer = new Stopwatch();
+				timer.Start();
+			}
+
+			return timer;
 		}
 	}
 }
