043886a6f725a52f6939eb569d7429bc4b6d06f8
Kyle Conlon <conlonkyle@gmail.com>
2021-01-09 11:31:44 -0500

    Modified the load of the EditHops page to avoid error on load due to the Hop property being null and the EditForm component requiring a non-null element.
    Added HttpClient mocking to the ClientUnitTests project. Test is currently checking what amount to static elements so need to update the asserts there to be useful.

---
+++ BrewJournal/ClientUnitTests/Helpers/MockHttpClientHelper.cs
@@ -0,0 +1,73 @@
+﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using RichardSzalay.MockHttp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClientUnitTests
+{
+	/// <summary>
+	/// Helper extension class to configure the HttpClient mock utilizing Richard Szalay's package.
+	/// </summary>
+	public static class MockHttpClientHelper
+	{
+		/// <summary>
+		/// Bunit.TestServiceProvider extension method. Instantiates and registers the MockHttpClient handler.
+		/// </summary>
+		/// <param name="services">The current test context</param>
+		public static MockHttpMessageHandler AddMockHttpClient(this TestServiceProvider services)
+		{
+			var mockHttpHandler = new MockHttpMessageHandler();
+			var httpClient = mockHttpHandler.ToHttpClient();
+			httpClient.BaseAddress = new Uri("http://localhost");
+			services.AddSingleton(httpClient);
+			return mockHttpHandler;
+		}
+
+		/// <summary>
+		/// Configures a 200 response to a mocked request by serializing the provided data object.
+		/// </summary>
+		/// <typeparam name="T">Data type of the provded content</typeparam>
+		/// <param name="request">The mock request object</param>
+		/// <param name="content"></param>
+		/// <returns></returns>
+		public static MockedRequest RespondSuccessJson<T>(this MockedRequest request, T content)
+		{
+			request.Respond(req =>
+			{
+				var response = new HttpResponseMessage(HttpStatusCode.OK);
+				response.Content = new StringContent(JsonSerializer.Serialize(content));
+				response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+				return response;
+			});
+			return request;
+		}
+
+		/// <summary>
+		/// Configures a 200 response to a mocked request by calling the provided function and serializing
+		/// the output.
+		/// </summary>
+		/// <typeparam name="T">Return type of the provided function</typeparam>
+		/// <param name="request">Mock request object</param>
+		/// <param name="contentProvider">Delegate function to call to provide request results</param>
+		/// <returns></returns>
+		public static MockedRequest RespondSuccessJson<T>(this MockedRequest request, Func<T> contentProvider)
+		{
+			request.Respond(req =>
+			{
+				var response = new HttpResponseMessage(HttpStatusCode.OK);
+				response.Content = new StringContent(JsonSerializer.Serialize(contentProvider()));
+				response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+				return response;
+			});
+			return request;
+		}
+	}
+}
--- BrewJournal/ClientUnitTests/Pages/EditHopTests.cs
+++ BrewJournal/ClientUnitTests/Pages/EditHopTests.cs
@@ -2,26 +2,43 @@ using NUnit.Framework;
 using Bunit;
 using BrewJournal.Client.Pages;
 using ClientUnitTests.Properties;
+using RichardSzalay.MockHttp;
+using Models;
+using System.Collections.Generic;
 
 namespace ClientUnitTests
 {
 	public class EditHopTests : Bunit.TestContext
 	{
+		Bunit.TestContext _ctx;
+		MockHttpMessageHandler _mockHttp;
+
 		[SetUp]
 		public void Setup()
 		{
+			_ctx = new Bunit.TestContext();
+			_mockHttp = _ctx.Services.AddMockHttpClient();
 		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			_mockHttp.Clear();
+		}
+
 
 		[Test]
 		public void PageShouldLoadDefaultHopWhenNoIdIsProvided()
 		{
+			_mockHttp.When("/Hop").RespondSuccessJson(new List<Hop>());
+
 			//-- Render the page with no parameters
-			var page = RenderComponent<EditHop>();
+			var page = _ctx.RenderComponent<EditHop>();
 
 			//-- Assert that the component initialized as expected
 			Assert.AreEqual(0, page.Instance.Id, Resources.EditHopDefaultIDCheckFailed);
-			var header = page.Find("h1");
-			header.MarkupMatches("<h1>Add Hop</h1>", Resources.EditHopDefaultHeaderCheckFailed);
+			var noHopsFoundMessage = page.Find("p");
+			noHopsFoundMessage.MarkupMatches("<p>No hops entered into databases match these filters.</p>", Resources.EditHopDefaultHeaderCheckFailed);
 		}
 	}
 }
